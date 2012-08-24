b86160ac861cc1e5d91427c81fa367e231ad9ab3
Morten Kjaergaard <morten.kjargaard@prevas.dk>
2012-08-24 15:27:41 +0200

    expanded static_scope to several arguments

--- include/llog/static_context.hpp
+++ include/llog/static_context.hpp
@@ -13,6 +13,27 @@ struct StaticScopeImpl
   {
   }
 
+  template<typename T1>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1)
+  {
+  }
+
+  template<typename T1, typename T2>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1,
+		   const char* name2, const Argument<T2>& argument2)
+  {
+  }
+
+  template<typename T1, typename T2, typename T3>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1,
+		   const char* name2, const Argument<T2>& argument2,
+		   const char* name3, const Argument<T3>& argument3)
+  {
+  }
+
 };
 
 template<int S>
@@ -22,6 +43,36 @@ struct StaticScopeImpl<true, S>
   {
     llog::llog<S>(event);
   }
+
+  template<typename T1>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1)
+  {
+    llog::llog<S>(event,
+		  name1, argument1);
+  }
+
+  template<typename T1, typename T2>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1,
+		   const char* name2, const Argument<T2>& argument2)
+  {
+    llog::llog<S>(event,
+		  name1, argument1,
+		  name2, argument2);
+  }
+
+  template<typename T1, typename T2, typename T3>
+  static void llog(const char* event,
+		   const char* name1, const Argument<T1>& argument1,
+		   const char* name2, const Argument<T2>& argument2,
+		   const char* name3, const Argument<T3>& argument3)
+  {
+    llog::llog<S>(event,
+		  name1, argument1,
+		  name2, argument2,
+		  name3, argument3);
+  }
 };
 
 template<int SeverityEnabled>
@@ -30,11 +81,44 @@ struct StaticScope
   template<int Severity>
   void llog(const char* event)
   {
-    StaticScopeImpl<Severity <= SeverityEnabled, Severity>::llog(event);
+    StaticScopeImpl<Severity <= SeverityEnabled, Severity>::
+      llog(event);
   }
+
+  template<int Severity, typename T1>
+  void llog(const char* event,
+	    const char* name1, const Argument<T1>& argument1)
+  {
+    StaticScopeImpl<Severity <= SeverityEnabled, Severity>::
+      llog(event,
+	   name1, argument1);
+  }
+
+  template<int Severity, typename T1, typename T2>
+  void llog(const char* event,
+	    const char* name1, const Argument<T1>& argument1,
+	    const char* name2, const Argument<T2>& argument2)
+  {
+    StaticScopeImpl<Severity <= SeverityEnabled, Severity>::
+      llog(event,
+	   name1, argument1,
+	   name2, argument2);
+  }
+
+  template<int Severity, typename T1, typename T2, typename T3>
+  void llog(const char* event,
+	    const char* name1, const Argument<T1>& argument1,
+	    const char* name2, const Argument<T2>& argument2,
+	    const char* name3, const Argument<T3>& argument3)
+  {
+    StaticScopeImpl<Severity <= SeverityEnabled, Severity>::
+      llog(event,
+	   name1, argument1,
+	   name2, argument2,
+	   name3, argument3);
+  }
+
 
 };
 
 }
-
-
